294e2c5d1add3875611dec95b19c03fb9864de4d
Nurmira16 <nurmirajorobek@gmail.com>
2025-10-15 21:28:06 +0200

    feat: add header and basic styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='header'>
+            <h1>Trivia Data Visualizer</h1>
+            <p>Based on Open Trivia DB</p>
+        </header>
     );
 };
 
